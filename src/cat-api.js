2bfec4fd2300911348956bafdbc31bab147aac11
oksanamatviyiv <113504985+oksanamatviyiv@users.noreply.github.com>
2023-11-21 03:21:05 +0200

    update

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,23 +2,36 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
     'live_JTWhR9cZ6bfUdmBOUDkNKkE5YeWsZDoJqKInjO3z60xayayEcz1WJttOZc5WF8Zr';
 
-export async function fetchCatByBreed(breedId) {
-  try {
-    const imageUrlResponse = await axios.get(
-      `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
-    );
-    const breedDetailResponse = await axios.get(
-      `https://api.thecatapi.com/v1/breeds/${breedId}`
-    );
-
-    return {
-      imageUrl: imageUrlResponse.data[0]?.url,
-      breedDetails: breedDetailResponse.data,
-    };
-  } catch (error) {
+export function fetchCatByBreed(breedId) {
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(imageUrlResponse => {
+      return axios
+        .get(`https://api.thecatapi.com/v1/breeds/${breedId}`)
+        .then(breedDetailResponse => ({
+          imageUrl: imageUrlResponse.data[0]?.url,
+          breedDetails: breedDetailResponse.data,
+        }));
+    })
+    .catch(error => {
       Notiflix.Notify.failure(
         `Oops! Something went wrong! Try reloading the page!`
       );
-    return { imageUrl: '', breedDetails: null };
-  }
+      return { imageUrl: '', breedDetails: null };
+    });
 }
+
+export function fetchBreeds() {
+  return axios.get('https://api.thecatapi.com/v1/breeds')
+    .then(response => response.data)
+    .catch(error => {
+      Notiflix.Notify.failure(
+        `Oops! Something went wrong! Try reloading the page!`
+);
+      return [];
+    });
+};
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,57 +1,57 @@
 import axios from 'axios';
 import { fetchCatByBreed } from './cat-api.js';
+import { fetchBreeds } from './cat-api.js';
 import SlimSelect from 'slim-select';
 new SlimSelect({
   select: `#breed-select`,
 });
 import Notiflix from 'notiflix';
 
-export async function fetchBreeds() {
-  try {
-    const response = await axios.get('https://api.thecatapi.com/v1/breeds');
-    return response.data;
-  }
-}
 const fetchBreedSelect = document.querySelector('.breed-select');
 const catInfoDiv = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 
-async function populateBreedSelect() {
-  try {
-    showLoader();
-    const breeds = await fetchBreeds();
-    breeds.forEach(breed => {
-      const option = document.createElement('option');
-      option.value = breed.id;
-      option.textContent = breed.name;
-      fetchBreedSelect.appendChild(option);
+function populateBreedSelect() {
+  
+  showLoader();
+  
+  fetchBreeds()
+    .then(breeds => {
+      breeds.forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed.id;
+        option.textContent = breed.name;
+        fetchBreedSelect.appendChild(option);
+      });
+      hideLoader();
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(`Oops! Something went wrong! Try reloading the page!`);
     });
-    hideLoader();
-  } catch (error) {
-    Notiflix.Notify.failure(`Oops! Something went wrong! Try reloading the page!`)
-  }
 }
-fetchBreedSelect.addEventListener('change', async event => {
+fetchBreedSelect.addEventListener('change', event => {
   const breedId = event.target.value;
-  try {
-    showLoader();
-    const { imageUrl, breedDetails } = await fetchCatByBreed(breedId);
-
-    if (breedDetails) {
-      catInfoDiv.innerHTML = `
-        <h2>${breedDetails.name}</h2>
-        <p>${breedDetails.description}</p>
-        <p>${breedDetails.temperament}</p>
-        <img src="${imageUrl}" alt="${breedDetails.name}" width="580px" />
-      `;
-    }
-    hideLoader(error);
-     Notiflix.Notify.info(`Loading data, please wait...`)
-  } catch (error) {
-    Notiflix.Notify.failure(`Oops! Something went wrong! Try reloading the page!`)
-  }
+    showLoader(breedId);
+  fetchCatByBreed(breedId)
+    .then(({ imageUrl, breedDetails }) => {
+      if (breedDetails) {
+        catInfoDiv.innerHTML = `
+          <h2>${breedDetails.name}</h2>
+          <p>${breedDetails.description}</p>
+          <p>${breedDetails.temperament}</p>
+          <img src="${imageUrl}" alt="${breedDetails.name}" width="580px" />
+        `;
+      }
+      hideLoader(); 
+      Notiflix.Notify.info(`Loading data, please wait...`);
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(`Oops! Something went wrong! Try reloading the page!`);
+    });
 });
+
+populateBreedSelect();
 
 function showLoader() {
   loader.style.display = 'block';
@@ -63,4 +63,5 @@ function hideLoader() {
   loader.style.display = 'none';
 }
 
-populateBreedSelect();
+
+
